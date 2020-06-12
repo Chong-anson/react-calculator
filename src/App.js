a5b6460b68d3a8e6545befc964181e86e203a699
anson <ansonchongwus44@gmail.com>
2020-06-11 22:32:54 -0700

    griddddddd

--- src/App.js
+++ src/App.js
@@ -71,36 +71,24 @@ function App() {
   return (
     <div className="App">
       <div className="calculator">
-        <div className="input" id="input">{ symbol === "" ? value : secondValue}</div>
+        <div className="display">{ symbol === "" ? value : secondValue}</div>
         <div className="buttons">
-          <div className="operators">
-            <div onClick={symbolChange("+")}>+</div>
-            <div onClick={symbolChange("-")}>-</div>
-            <div onClick={symbolChange("*")}>&times;</div>
-            <div onClick={symbolChange("/")}>&divide;</div>
-          </div>
-          <div className="leftPanel">
-            <div className="numbers">
-              <div onClick={updateValue("7")}>7</div>
-              <div onClick={updateValue("8")}>8</div>
-              <div onClick={updateValue("9")}>9</div>
-            </div>
-            <div className="numbers">
-              <div onClick={updateValue("4")}>4</div>
-              <div onClick={updateValue("5")}>5</div>
-              <div onClick={updateValue("6")}>6</div>
-            </div>
-            <div className="numbers">
-              <div onClick={updateValue("1")}>1</div>
-              <div onClick={updateValue("2")}>2</div>
-              <div onClick={updateValue("3")}>3</div>
-            </div>
-            <div className="numbers">
-              <div onClick={updateValue("0")}>0</div>
-              <div onClick={updateValue(".")}>.</div>
-              <div id="clear" onClick={clear}>C</div>
-            </div>
-          </div>
+          <div className="symbol" onClick={symbolChange("+")}>+</div>
+          <div className="symbol" onClick={symbolChange("-")}>-</div>
+          <div className="symbol" onClick={symbolChange("*")}>&times;</div>
+          <div className="symbol" onClick={symbolChange("/")}>&divide;</div>
+          <div onClick={updateValue("7")}>7</div>
+          <div onClick={updateValue("8")}>8</div>
+          <div onClick={updateValue("9")}>9</div>
+          <div onClick={updateValue("4")}>4</div>
+          <div onClick={updateValue("5")}>5</div>
+          <div onClick={updateValue("6")}>6</div>
+          <div onClick={updateValue("1")}>1</div>
+          <div onClick={updateValue("2")}>2</div>
+          <div onClick={updateValue("3")}>3</div>
+          <div onClick={updateValue("0")}>0</div>
+          <div onClick={updateValue(".")}>.</div>
+          <div id="clear" onClick={clear}>C</div>
           <div className="equal" id="result" onClick={result}>=</div>
         </div>
       </div>
